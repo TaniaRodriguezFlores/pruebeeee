3dc4679040976b84259d4da9dea1519ca07e4579
TaniaRodriguezFlores <49164582+TaniaRodriguezFlores@users.noreply.github.com>
2025-02-15 09:56:10 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Pruebas
+//
+//  Created by Tania Rodriguez Flores on 05/02/25.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    /*char C=128;
+    printf("%lu\n",sizeof(C));
+    printf("%d",C);*/
+    int a=5, b=7;
+    int r=pow(5,7);
+    return 0;
+}
